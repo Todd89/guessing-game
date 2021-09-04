d0b6cee70e4b5e9e7baa8a95eb2f2ddee9a0e35d
Todd89 <alex-89fish@tut.by>
2021-09-04 22:47:09 +0300

    add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,34 @@
 class GuessingGame {
-    constructor() {}
+
+    min = 0;
+    max = 0;
+    number = 409;
+    mid = 0;
+    constructor() {
+
+    }
 
     setRange(min, max) {
+
+         this.min = min;
+         this.max = max;
+		console.log (min, max)
 
     }
 
     guess() {
 
+        this.mid = Math.ceil ((this.min + this.max) / 2);
+            return this.mid;
     }
 
     lower() {
-
+        return  this.max = this.mid;
+        
     }
 
     greater() {
+      return  this.min = this.mid;
 
     }
 }
